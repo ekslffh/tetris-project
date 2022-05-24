2a0ea4f39769ff57ae0bfe7402774a3b3d13e183
= <=>
2022-05-24 10:11:42 +0900

    add KeyListener

--- src/ch1/TetrisGame.java
+++ src/ch1/TetrisGame.java
@@ -1,11 +1,13 @@
 package ch1;
 
 import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
-public class TetrisGame extends JFrame {
+public class TetrisGame extends JFrame implements KeyListener {
 
 	private GameHandler handler;
 	private JTextArea textArea = new JTextArea();
@@ -16,6 +18,7 @@ public class TetrisGame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null); // this will center your apps
 		textArea.setFont(new Font("courier New", Font.PLAIN, 30));
+		textArea.addKeyListener(this);
 		add(textArea);
 		textArea.setEditable(false);
 		setVisible(true);
@@ -48,6 +51,24 @@ public class TetrisGame extends JFrame {
 		}
 		
 		// 2. Get Input ==========================
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
